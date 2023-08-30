1455c4daf8c096d9a71cbe308a96cb4857bc4205
Andreas <andreas@ohmy.co>
2023-08-30 09:46:57 +0200

    0.1.4 - use proper tuple when calling crop

--- setup.py
+++ setup.py
@@ -1,31 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='wagtail-thumbify',
-    version='0.1.3',
-
-    install_requires=[
-        'libthumbor',
-        'Pillow'
-    ],
-
-    description='Thumbify for Wagtail.',
-
-    author='Andreas Sundström',
-    author_email='andreas@ohmy.se',
-
-    url='http://github.com/Oh-My/wagtail-thumbify',
-
+    name="wagtail-thumbify",
+    version="0.1.4",
+    install_requires=["libthumbor", "Pillow"],
+    description="Thumbify for Wagtail.",
+    author="Andreas Sundström",
+    author_email="andreas@ohmy.se",
+    url="http://github.com/Oh-My/wagtail-thumbify",
     zip_safe=True,
-
     packages=find_packages(),
-
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Framework :: Django',
-    ]
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ],
 )
--- wagtail_thumbify/templatetags/thumbify.py
+++ wagtail_thumbify/templatetags/thumbify.py
@@ -5,7 +5,10 @@ from django.conf import settings
 from django.forms.utils import flatatt
 from django.utils.safestring import mark_safe, SafeString
 
-from wagtail.images.templatetags.wagtailimages_tags import image as wagtail_image_tag, allowed_filter_pattern
+from wagtail.images.templatetags.wagtailimages_tags import (
+    image as wagtail_image_tag,
+    allowed_filter_pattern,
+)
 from wagtail.images.models import Image as WagtailImage
 from libthumbor import CryptoURL
 import re
@@ -22,36 +25,38 @@ register = template.Library()
 
 crypto = CryptoURL(key=settings.THUMBOR_SECURITY_KEY)
 
-logger = logging.getLogger('django')
+logger = logging.getLogger("django")
+
 
 class ThumbifyImageNode(template.Node):
     thumbor_filters_map = {
-        'brightness': 'brightness',
-        'background_color': 'background_color',
-        'blur': 'blur',
-        'contrast': 'contrast',
-        'convolution': 'convolution',
-        'cover': 'cover',
-        'equalize': 'equalize',
+        "brightness": "brightness",
+        "background_color": "background_color",
+        "blur": "blur",
+        "contrast": "contrast",
+        "convolution": "convolution",
+        "cover": "cover",
+        "equalize": "equalize",
         #'fill': 'fill',
-        'focal': 'focal',
+        "focal": "focal",
         #'format': 'format',
-        'grayscale': 'grayscale',
-        'max_bytes': 'max_bytes',
-        'no_upscale': 'no_upscale',
-        'proportion': 'proportion',
-        'quality': 'quality',
-        'rgb': 'rgb',
-        'rotate': 'rotate',
-        'round_corner': 'round_corner',
-        'saturation': 'saturation',
-        'sharpen': 'sharpen',
-        'stretch': 'stretch',
-        'strip_exif': 'strip\\_exif',
-        'strip_icc': 'strip\\_icc',
-        'upscale': 'upsacle',
-        'watermark': 'watermark'
+        "grayscale": "grayscale",
+        "max_bytes": "max_bytes",
+        "no_upscale": "no_upscale",
+        "proportion": "proportion",
+        "quality": "quality",
+        "rgb": "rgb",
+        "rotate": "rotate",
+        "round_corner": "round_corner",
+        "saturation": "saturation",
+        "sharpen": "sharpen",
+        "stretch": "stretch",
+        "strip_exif": "strip\\_exif",
+        "strip_icc": "strip\\_icc",
+        "upscale": "upsacle",
+        "watermark": "watermark",
     }
+
     def __init__(self, image_expr, filter_spec, output_var_name=None, attrs={}):
         self.image_expr = image_expr
         self.output_var_name = output_var_name
@@ -62,43 +67,51 @@ class ThumbifyImageNode(template.Node):
         try:
             image = self.image_expr.resolve(context)
         except template.VariableDoesNotExist:
-            return ''
+            return ""
 
         if not image:
             if self.output_var_name:
                 context[self.output_var_name] = None
-            return ''
+            return ""
 
         alt_text = None
         filter_kwargs = self.get_filter_kwargs()
 
-        if settings.THUMBOR_USE and settings.THUMBOR_SECURITY_KEY and settings.THUMBOR_SERVER:
-
+        if (
+            settings.THUMBOR_USE
+            and settings.THUMBOR_SECURITY_KEY
+            and settings.THUMBOR_SERVER
+        ):
             if type(image) == SafeString:
                 image_url = str(image)
             elif type(image) == str:
                 image_url = image
             else:
-                image_url = image.get_rendition('original').url
+                image_url = image.get_rendition("original").url
                 alt_text = image.default_alt_text
 
             # Try to get absolute url if image_url does not start with http(s)
-            if not re.match(r'^https?://', image_url):
-                if hasattr(context, 'request'):
+            if not re.match(r"^https?://", image_url):
+                if hasattr(context, "request"):
                     image_url = context.request.build_absolute_uri(image_url)
-                elif (context.get('request')):
-                    image_url = context['request'].build_absolute_uri(image_url)
+                elif context.get("request"):
+                    image_url = context["request"].build_absolute_uri(image_url)
 
-            if getattr(settings, 'THUMBOR_IMAGE_URL_REPLACEMENT', False):
-                image_url = image_url.replace(settings.THUMBOR_IMAGE_URL_REPLACEMENT[0], settings.THUMBOR_IMAGE_URL_REPLACEMENT[1])
+            if getattr(settings, "THUMBOR_IMAGE_URL_REPLACEMENT", False):
+                image_url = image_url.replace(
+                    settings.THUMBOR_IMAGE_URL_REPLACEMENT[0],
+                    settings.THUMBOR_IMAGE_URL_REPLACEMENT[1],
+                )
 
-            if getattr(settings, 'THUMBOR_QUOTE_URL', False):
+            if getattr(settings, "THUMBOR_QUOTE_URL", False):
                 image_url = urllib.parse.quote(image_url)
 
-            filter_kwargs.update({
-                'image_url': image_url,
-                'smart': True,
-            })
+            filter_kwargs.update(
+                {
+                    "image_url": image_url,
+                    "smart": True,
+                }
+            )
 
             thumbor_url = crypto.generate(**filter_kwargs)
             processed_image_url = settings.THUMBOR_SERVER + thumbor_url
@@ -112,22 +125,24 @@ class ThumbifyImageNode(template.Node):
                 original_image = image
                 alt_text = image.default_alt_text
 
-            processed_image_url = self.process_image(image=original_image, **filter_kwargs)
+            processed_image_url = self.process_image(
+                image=original_image, **filter_kwargs
+            )
 
         if self.output_var_name:
-            context[self.output_var_name] = {'url': processed_image_url}
-            return ''
+            context[self.output_var_name] = {"url": processed_image_url}
+            return ""
         else:
             resolved_attrs = {}
             for key in self.attrs:
                 resolved_attrs[key] = self.attrs[key].resolve(context)
 
             if alt_text:
-                resolved_attrs.update({
-                    'alt': alt_text
-                })
+                resolved_attrs.update({"alt": alt_text})
 
-            return mark_safe('<img src="{}"{}>'.format(processed_image_url, flatatt(resolved_attrs)))
+            return mark_safe(
+                '<img src="{}"{}>'.format(processed_image_url, flatatt(resolved_attrs))
+            )
 
     def get_filter_kwargs(self):
         filter_kwargs = {}
@@ -135,42 +150,42 @@ class ThumbifyImageNode(template.Node):
         filters = []
 
         for filter in self.filter_spec:
-            filter_parts = filter.split('-')
+            filter_parts = filter.split("-")
 
-            if filter_parts[0] == 'width':
-                filter_kwargs.update({'width': filter_parts[1]})
+            if filter_parts[0] == "width":
+                filter_kwargs.update({"width": filter_parts[1]})
 
-            if filter_parts[0] == 'height':
-                filter_kwargs.update({'height': filter_parts[1]})
+            if filter_parts[0] == "height":
+                filter_kwargs.update({"height": filter_parts[1]})
 
-            if filter_parts[0] == 'fill':
-                width, height = filter_parts[1].split('x')
-                filter_kwargs.update({'width': width})
-                filter_kwargs.update({'height': height})
-                filter_kwargs.update({'fit_in': False})
+            if filter_parts[0] == "fill":
+                width, height = filter_parts[1].split("x")
+                filter_kwargs.update({"width": width})
+                filter_kwargs.update({"height": height})
+                filter_kwargs.update({"fit_in": False})
 
-            if filter_parts[0] == 'max':
-                width, height = filter_parts[1].split('x')
-                filter_kwargs.update({'width': width})
-                filter_kwargs.update({'height': height})
-                filter_kwargs.update({'fit_in': True})
+            if filter_parts[0] == "max":
+                width, height = filter_parts[1].split("x")
+                filter_kwargs.update({"width": width})
+                filter_kwargs.update({"height": height})
+                filter_kwargs.update({"fit_in": True})
 
             if filter_parts[0] in self.thumbor_filters_map:
-                filter_parts = filter.split('-', maxsplit=1)
+                filter_parts = filter.split("-", maxsplit=1)
                 name = filter_parts[0]
                 value = None
                 if len(filter_parts) > 1:
                     value = filter_parts[1]
                 name = self.thumbor_filters_map[filter_parts[0]]
 
-                filters.append('%s(%s)' % (name, value or ''))
+                filters.append("%s(%s)" % (name, value or ""))
 
         if filters:
-            filter_kwargs['filters'] = filters
+            filter_kwargs["filters"] = filters
 
         return filter_kwargs
 
-    def process_image(self, image, width = None, height = None, fit_in = False):
+    def process_image(self, image, width=None, height=None, fit_in=False):
         if str(width).isnumeric():
             width = int(width)
         if str(height).isnumeric():
@@ -181,7 +196,9 @@ class ThumbifyImageNode(template.Node):
             height = 0
 
         if type(image) == WagtailImage:
-            original_image_path = str(Path(settings.MEDIA_ROOT).joinpath(image.file.name))
+            original_image_path = str(
+                Path(settings.MEDIA_ROOT).joinpath(image.file.name)
+            )
 
             stem = Path(original_image_path).stem
             src_hash = str(hashlib.sha1(original_image_path.encode()).hexdigest())
@@ -191,28 +208,36 @@ class ThumbifyImageNode(template.Node):
             src_hash = str(hashlib.sha1(image.encode()).hexdigest())
             suffix = Path(image).suffix
         else:
-            raise ValueError('Unexpected image value: {}'.format(type(image)))
+            raise ValueError("Unexpected image value: {}".format(type(image)))
 
-        filter_key = '{}x{}'.format(width, height)
+        filter_key = "{}x{}".format(width, height)
         if fit_in:
-            filter_key += '_fit_in'
+            filter_key += "_fit_in"
 
-        image_name = '{}_{}_{}{}'.format(src_hash, stem, filter_key, suffix)
+        image_name = "{}_{}_{}{}".format(src_hash, stem, filter_key, suffix)
 
-        resized_dir = Path(settings.MEDIA_ROOT).joinpath('resized').joinpath(src_hash[0:2])
+        resized_dir = (
+            Path(settings.MEDIA_ROOT).joinpath("resized").joinpath(src_hash[0:2])
+        )
         processed_image_filename = resized_dir.joinpath(image_name)
-        processed_image_url = Path(settings.MEDIA_URL).joinpath('resized').joinpath(src_hash[0:2]).joinpath(image_name)
+        processed_image_url = (
+            Path(settings.MEDIA_URL)
+            .joinpath("resized")
+            .joinpath(src_hash[0:2])
+            .joinpath(image_name)
+        )
 
         if processed_image_filename.exists():
             return str(processed_image_url)
 
-        with NamedTemporaryFile(mode='wb+') as tmp_image:
+        with NamedTemporaryFile(mode="wb+") as tmp_image:
             if type(image) == WagtailImage:
                 pil_image = Image.open(original_image_path)
             else:
-                if not re.match(r'^https?://', image):
-                    raise template.TemplateSyntaxError('Image parameter should be either an image object or an absolute URL')
-
+                if not re.match(r"^https?://", image):
+                    raise template.TemplateSyntaxError(
+                        "Image parameter should be either an image object or an absolute URL"
+                    )
 
                 r = requests.get(image, allow_redirects=True)
 
@@ -256,14 +281,14 @@ class ThumbifyImageNode(template.Node):
                     crop_width = round(pil_image.height * wanted_ratio)
                     crop_height = pil_image.height
 
-                box = {
-                    'left': round((pil_image.width - crop_width) / 2),
-                    'upper': round((pil_image.height - crop_height) / 2),
-                    'right': round((pil_image.width - crop_width) / 2) + crop_width,
-                    'lower': round((pil_image.height - crop_height) / 2) + crop_height,
-                }
+                box = (
+                    round((pil_image.width - crop_width) / 2),  # left
+                    round((pil_image.height - crop_height) / 2),  # upper
+                    round((pil_image.width - crop_width) / 2) + crop_width,  # right
+                    round((pil_image.height - crop_height) / 2) + crop_height,  # lower
+                )
 
-                pil_image = pil_image.crop(box.values())
+                pil_image = pil_image.crop(box)
 
                 # Then, resize to wanted dimensions
                 pil_image = pil_image.resize((width, height))
@@ -273,7 +298,7 @@ class ThumbifyImageNode(template.Node):
         return str(processed_image_url)
 
 
-@register.tag(name='thumbify')
+@register.tag(name="thumbify")
 def image(parser, token):
     bits = token.split_contents()[1:]
     image_expr = parser.compile_filter(bits[0])
@@ -287,7 +312,7 @@ def image(parser, token):
     is_valid = True
 
     for bit in bits:
-        if bit == 'as':
+        if bit == "as":
             # token is of the form {% image self.photo max-320x200 as img %}
             as_context = True
         elif as_context:
@@ -298,8 +323,10 @@ def image(parser, token):
                 is_valid = False
         else:
             try:
-                name, value = bit.split('=')
-                attrs[name] = parser.compile_filter(value)  # setup to resolve context variables as value
+                name, value = bit.split("=")
+                attrs[name] = parser.compile_filter(
+                    value
+                )  # setup to resolve context variables as value
             except ValueError:
                 if allowed_filter_pattern.match(bit):
                     filter_specs.append(bit)
@@ -330,10 +357,11 @@ def image(parser, token):
         )
 
     if is_valid:
-        return ThumbifyImageNode(image_expr, filter_specs, attrs=attrs, output_var_name=output_var_name)
+        return ThumbifyImageNode(
+            image_expr, filter_specs, attrs=attrs, output_var_name=output_var_name
+        )
     else:
         raise template.TemplateSyntaxError(
             "'image' tag should be of the form {% image self.photo max-320x200 [ custom-attr=\"value\" ... ] %} "
             "or {% image self.photo max-320x200 as img %}"
         )
-
